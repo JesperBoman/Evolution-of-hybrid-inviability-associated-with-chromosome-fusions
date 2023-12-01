596746365356b1e4b74c0bb992ff87c4c37d0412
JesperBoman <74507057+JesperBoman@users.noreply.github.com>
2023-12-01 14:52:09 +0100

    Create genotypeGVCFs.sh

---
+++ Scripts/fixed_differences/05_Variant_calling_postBQSR/genotypeGVCFs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+
+module load bioinfo-tools GATK/4.2.0.0
+
+id=$1
+scaffold=$2
+ref=$3
+
+
+gatk --java-options "-Xmx6g" GenotypeGVCFs \
+ -R $ref \
+ -V gendb://genomicsdb_postBQSR.$id.$scaffold\
+ -all-sites \
+ -L $scaffold \
+ -O $scaffold.dir/$id.$scaffold.allsites.vcf.gz
