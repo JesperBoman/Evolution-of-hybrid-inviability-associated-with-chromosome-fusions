7539c69318365ce87704a89d5e2d1ae1b12109b8
JesperBoman <74507057+JesperBoman@users.noreply.github.com>
2023-12-01 14:51:06 +0100

    Create run_genomicsDB.sh

---
+++ Scripts/fixed_differences/05_Variant_calling_postBQSR/run_genomicsDB.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+id="DToL"
+
+while IFS= read -r scaffold
+do
+
+#Building genomics DB
+sbatch -J genomicsDB.$id.$scaffold -o slurm/genomicsDB.$id.$scaffold.output -e slurm/genomicsDB.$id.$scaffold.error  -A "Project ID" -t 0-03:00:00 -p core -n 1 genomicsDB.sh $id $scaffold
+
+done < "scaffolds.list"
